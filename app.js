bd9ab03c2ca1f8a01775c7d9cc3c261eb92cd7c7
vladimir <lovkiy2012@gmail.com>
2020-10-13 22:08:45 +0300

    conected mongodb

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('app33');
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = config.get('port') || 5000;
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDb conected...');
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+  } catch (error) {
+    console.log('Server Error', error.message);
+    process.exit(1);
+  }
+}
+start();
+
+// dy8VgZrMmVatrWXk
