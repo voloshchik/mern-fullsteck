97f8b5e410c907cd9cf2035614ff9a02c76a254a
vladimir <lovkiy2012@gmail.com>
2020-10-23 22:49:51 +0300

    test

---
+++ client/build/precache-manifest.272a459cc41b4b1122ecae61f2bf02cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89624928473e040494d01f9211843525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e724e108ac843eac3a",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "4c9f11640cbf993e54c0",
+    "url": "/static/css/main.df93d367.chunk.css"
+  },
+  {
+    "revision": "65e724e108ac843eac3a",
+    "url": "/static/js/2.14849817.chunk.js"
+  },
+  {
+    "revision": "d57f9039b732224c280a27b57e79efde",
+    "url": "/static/js/2.14849817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9f11640cbf993e54c0",
+    "url": "/static/js/main.fdc7142a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
